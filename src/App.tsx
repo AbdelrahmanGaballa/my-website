c4ec990f1c2ca1b08eeb26e6b131ee0ba30383ec
AbdelrahmanGaballa <bidogaballa11@gmail.com>
2026-07-05 22:16:17 -0400

    weewqewqwqc

--- src/App.tsx
+++ src/App.tsx
@@ -85,8 +85,8 @@ export default function App() {
 
       {/* Header */}
 <header
-  className="sticky top-0 z-40 border-b border-red-900/40
-             bg-red-900/90 bg-gradient-to-r from-red-950/30 via-red-800/30 to-red-950/30
+  className="sticky top-0 z-40 border-b border-red-900/30
+             bg-red-950/90 bg-gradient-to-r from-red-950/20 via-red-800/40 to-red-950/20
              text-white backdrop-blur-md transition-all duration-300"
 >
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
